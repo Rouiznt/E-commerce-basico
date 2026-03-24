f0b85c93a94107d84e10561ba5cd045fc831ffdf
Santiago Ruiz <santiagoruizsalazar123@gmail.com>
2026-03-24 15:57:19 -0500

    añado la funcion de filtrar por descuento

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   const [error, setError] = useState(null);
 
   const [categoria, setCategoria] = useState("all");
+  const [soloDescuento, setSoloDescuento] = useState(false);
 
   useEffect(() => {
     fetch("https://dummyjson.com/products")
@@ -25,26 +26,38 @@ function App() {
       });
   }, []);
 
-  if (loading) return <h2>Cargando</h2>;
+  if (loading) return <h2>Cargando productos...</h2>;
   if (error) return <h2>{error}</h2>;
 
+  // filtros
+  const productosFiltrados = productos
+    .filter((p) => categoria === "all" || p.category === categoria)
+    .filter((p) => !soloDescuento || p.discountPercentage > 0);
 
-  const productosFiltrados =
-    categoria === "all"
-      ? productos
-      : productos.filter((p) => p.category === categoria);
-
+  // catgorias
   const categorias = ["all", ...new Set(productos.map(p => p.category))];
 
   return (
     <div>
       <h1>Productos</h1>
 
+      {/* Filtro por categoría */}
       <CategoryFilter
         categorias={categorias}
         setCategoria={setCategoria}
       />
 
+      {/* Filtro por descuento */}
+      <label>
+        <input
+          type="checkbox"
+          checked={soloDescuento}
+          onChange={(e) => setSoloDescuento(e.target.checked)}
+        />
+        Mostrar solo productos con descuento
+      </label>
+
+      {/* Lista de productos */}
       <div className="productos">
         {productosFiltrados.map((producto) => (
           <ProductCard key={producto.id} producto={producto} />
